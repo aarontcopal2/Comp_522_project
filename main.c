162a523c6357206139aaf5cea50c6d3f68820daa
Aaron Cherian <atc8@iris.cs.rice.edu>
2020-11-25 18:22:52 -0600

    code for main.c. This will be the microbenchmark for our hashtable datastructure

--- main.c
+++ main.c
@@ -4,7 +4,31 @@
 
 #include "hashtable/Split-Ordered-Lists.h"
 
-int main () {
 
+
+//******************************************************************************
+// type definitions
+//******************************************************************************
+
+typedef struct {
+    int apartment_no;
+    char *street_name;
+} address;
+
+
+
+//******************************************************************************
+// interface operations
+//******************************************************************************
+
+int main () {
+    bool status;
+
+    initialize_hashtable();
+
+    so_key_t key = 1;
+    address addr = {232, "Maroneal Street"}; 
+    val_t val = (void*)&addr;
+    status = map_insert(key, val);
     
 }

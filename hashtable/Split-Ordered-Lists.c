3feefc60573b7fc2070163ead6210f80c324f498
Aaron Cherian <atc8@iris.cs.rice.edu>
2020-11-27 13:52:59 -0600

    adding print statements and additional key attribute inside NodeType for debug purposes. Mallocing the ST bucket variable inside intialize_hashtable(). free() replaced with retire_node() inside list api. TO-DO: add replace code based on hazard pointers

--- hashtable/Split-Ordered-Lists.c
+++ hashtable/Split-Ordered-Lists.c
@@ -15,6 +15,7 @@
 
 #include <stdlib.h>     // NULL
 #include <stdatomic.h>  // atomic_fetch_add
+#include <stdio.h>      // printf
 
 
 
@@ -41,8 +42,8 @@
 //******************************************************************************
 
 /* shared variables */
-typedef MarkPtrType* segment_t[SEGMENT_SIZE];
-segment_t ST[2];            // buckets (Note that the 100 is harcoded and should be made dynamic)
+typedef MarkPtrType *segment_t; //segment_t is an array of MarkType pointers
+segment_t *ST;              // buckets (2D array of Marktype pointers)
 atomic_ullong count = 0;    // total nodes in hash table
 uint size = 2;              // hash table size
 
@@ -87,7 +88,8 @@ static bool is_dummy_node(so_key_t key) {
 }
 
 
-static MarkPtrType* get_bucket(uint bucket) {
+static MarkPtrType get_bucket(uint bucket) {
+    printf("get_bucket: %d\n", bucket);
     uint segment = bucket / SEGMENT_SIZE;
 
     // bucket not initialized, hence segment is NULL
@@ -99,6 +101,7 @@ static MarkPtrType* get_bucket(uint bucket) {
 
 
 static void set_bucket(uint bucket, NodeType *head) {
+    printf("set_bucket: %d\n", bucket);
     uint segment = bucket / SEGMENT_SIZE;
     MarkPtrType *null_segment = (MarkPtrType*)calloc(sizeof(MarkPtrType)*SEGMENT_SIZE, 0);
 
@@ -109,16 +112,17 @@ static void set_bucket(uint bucket, NodeType *head) {
             new_segment[i] = UNINITIALIZED;
         }*/
         
-        if(!atomic_compare_exchange_strong(ST[segment], null_segment, new_segment)) {
+        if(!atomic_compare_exchange_strong(&ST[segment], null_segment, new_segment)) {
             free(new_segment);
         }
     }
     free(null_segment);
-    ST[segment][bucket % SEGMENT_SIZE] = &head;
+    ST[segment][bucket % SEGMENT_SIZE] = head;
 }
 
 
 static uint get_parent(uint bucket) {
+    printf("get_parent: %d\n", bucket);
     // parent will differ with child bucket at 1st 1bit of child from left
     // parent will have that bit set to 0
     for (int i = 31; i >= 0; ++i) {
@@ -131,25 +135,32 @@ static uint get_parent(uint bucket) {
 
 
 static void initialize_bucket(uint bucket) {
+    printf("initialize_bucket: %d\n", bucket);
+
     MarkPtrType cur;
     uint parent = get_parent(bucket);
-    MarkPtrType *parent_bucket_ptr = get_bucket(parent);
+    MarkPtrType parent_bucket_ptr = get_bucket(parent);
     if (parent_bucket_ptr == UNINITIALIZED) {
         initialize_bucket(parent);
     }
+    printf("test1\n");
     NodeType *dummy = malloc(sizeof(NodeType));
-    dummy->key = so_dummy_key(bucket);      // is this param correct?
+    dummy->so_key = so_dummy_key(bucket);      // is this param correct?
+    dummy->key = bucket;
+    dummy->next = NULL;
     // do we need to save the hash inside the node?
 
     /* if another thread began initialization of the same bucket, but didnt complete then adding dummy again will fail
     * if so, we delete allocated dummy node of current thread and instead use the dummy node of the successful thread(cur points to the dummy node of that thread) */
     
-
+    printf("test2\n");
     if (!list_insert(parent_bucket_ptr, dummy)) {
-        free(dummy);
+        retire_node(dummy);
         dummy = cur;
     }
+    printf("test3\n");
     set_bucket(bucket, dummy);
+    printf("test4\n");
 }
 
 
@@ -169,10 +180,14 @@ static uint64_t fetch_and_decrement_count() {
 //******************************************************************************
 
 void initialize_hashtable () {
+    ST = malloc(sizeof(segment_t) * size);
+    ST[0] = (MarkPtrType*)malloc(SEGMENT_SIZE * sizeof(MarkPtrType*));
+
     // adding a dummy node for key = 0. Without this node, intialize bucket calls to bucket=0 will be stuck in an infinite loop
     t_key start_key = 0;
     NodeType *start_node = malloc(sizeof(NodeType));
-    start_node->key = so_dummy_key(0);
+    start_node->so_key = so_dummy_key(start_key);
+    start_node->key = start_key;
     start_node->next = NULL;
     set_bucket(start_key, start_node);
     //ST[0][0] = &node;
@@ -180,16 +195,18 @@ void initialize_hashtable () {
 
 
 bool map_insert(t_key key, val_t val) {
+    printf("map_insert: %d\n", key);
     // inside the node, key is stored in split-ordered form
     NodeType *node = malloc(sizeof(NodeType));
-    node->key = so_regular_key(key);
+    node->so_key = so_regular_key(key);
+    node->key = key;
     node->val = val;
     // do we need to save the hash inside the node?
 
     uint bucket = key % size;
 
     // intialize bucket if not already done
-    MarkPtrType *bucket_ptr = get_bucket(bucket);
+    MarkPtrType bucket_ptr = get_bucket(bucket);
     if (bucket_ptr == UNINITIALIZED) {
         initialize_bucket(bucket);
     }
@@ -214,7 +231,7 @@ bool map_insert(t_key key, val_t val) {
 bool map_find(t_key key) {
     uint bucket = key % size;
 
-    MarkPtrType *bucket_ptr = get_bucket(bucket);
+    MarkPtrType bucket_ptr = get_bucket(bucket);
     // ensure that bucket is initialized
     if (bucket_ptr == UNINITIALIZED) {
         initialize_bucket(bucket);
@@ -226,7 +243,7 @@ bool map_find(t_key key) {
 bool map_delete(t_key key) {
     uint bucket = key % size;
 
-    MarkPtrType *bucket_ptr = get_bucket(bucket);
+    MarkPtrType bucket_ptr = get_bucket(bucket);
     // ensure that bucket is initialized
     if (bucket_ptr == UNINITIALIZED) {
         initialize_bucket(bucket);

23e554d91dd385c7de85abd58e65f21555f9b901
Aaron Cherian <atc8@iris.cs.rice.edu>
2020-11-27 21:44:20 -0600

    delete operation works

--- main.c
+++ main.c
@@ -27,6 +27,10 @@ typedef struct {
 //******************************************************************************
 
 static void print_address(address *addr) {
+    if (!addr) {
+        printf("address passed is NULL\n");
+        return;
+    }
     printf("address: {\n\tapartment_no: %d,\n\tstreet_name: %s\n}\n", 
         addr->apartment_no, addr->street_name);
 }
@@ -45,10 +49,19 @@ int main () {
     address addr = {232, "Maroneal Street"}; 
     val_t val = (void*)&addr;
 
+    printf("\n***********map_insert(1)***********\n");
     status = map_insert(key, val);
-    printf("status map_insert(1) %d\n", status);
+    printf("***********status: %d***********\n", status);
     
-    printf("map_search(1)\n");
+    printf("***********map_search(1)***********\n");
     address *result_val = (address*) map_search(key);
+    print_address(result_val);
+
+    printf("***********map_delete(1)***********\n");
+    status = map_delete(key);
+    printf("***********status: %d***********\n", status);
+
+    printf("***********map_search(1)***********\n");
+    result_val = (address*) map_search(key);
     print_address(result_val);
 }

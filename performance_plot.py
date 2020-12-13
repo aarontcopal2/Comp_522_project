d63ce127309041d13b47c4ef990a797c8580e029
Aaron Cherian <aarontcopal2@gmail.com>
2020-12-13 16:45:22 -0600

    added x,ylabels to plots

--- performance_plot.py
+++ performance_plot.py
@@ -32,26 +32,31 @@ fontsize = 10
 ax.plot(threads,splay_benchmark[0],c='b',marker="^",ls='--',label='Lock-based splay tree',fillstyle='none')
 ax.plot(threads,sol_benchmark[0],c='m',marker="o",ls='--',label='Lock-Free hash table',fillstyle='none')
 ax.set_title("50% inserts, 0% finds, 50% deletes",fontsize=fontsize)
+ax.set(xlabel="threads", ylabel="Time(sec)")
 
 ax = plt.subplot2grid((3,4), (0, 2), colspan=2)
 ax.plot(threads,splay_benchmark[1],c='b',marker="^",ls='--',label='Lock-based splay tree',fillstyle='none')
 ax.plot(threads,sol_benchmark[1],c='m',marker="o",ls='--',label='Lock-Free hash table',fillstyle='none')
 ax.set_title("33% inserts, 33% finds, 33% deletes",fontsize=fontsize)
+ax.set(xlabel="threads", ylabel="Time(sec)")
 
 ax = plt.subplot2grid((3,4), (1, 0), colspan=2)
 ax.plot(threads,splay_benchmark[2],c='b',marker="^",ls='--',label='Lock-based splay tree',fillstyle='none')
 ax.plot(threads,sol_benchmark[2],c='m',marker="o",ls='--',label='Lock-Free hash table',fillstyle='none')
 ax.set_title("25% inserts, 50% finds, 25% deletes",fontsize=fontsize)
+ax.set(xlabel="threads", ylabel="Time(sec)")
 
 ax = plt.subplot2grid((3,4), (1, 2), colspan=2)
 ax.plot(threads,splay_benchmark[3],c='b',marker="^",ls='--',label='Lock-based splay tree',fillstyle='none')
 ax.plot(threads,sol_benchmark[3],c='m',marker="o",ls='--',label='Lock-Free hash table',fillstyle='none')
 ax.set_title("15% inserts, 70% finds, 15% deletes",fontsize=fontsize)
+ax.set(xlabel="threads", ylabel="Time(sec)")
 
 ax = plt.subplot2grid((3,4), (2, 1), colspan=2)
 ax.plot(threads,splay_benchmark[4],c='b',marker="^",ls='--',label='Lock-based splay tree',fillstyle='none')
 ax.plot(threads,sol_benchmark[4],c='m',marker="o",ls='--',label='Lock-Free hash table',fillstyle='none')
 ax.set_title("5% inserts, 90% finds, 5% deletes",fontsize=fontsize)
+ax.set(xlabel="threads", ylabel="Time(sec)")
 ax.set
 fig.tight_layout()
 handles, labels = ax.get_legend_handles_labels()
